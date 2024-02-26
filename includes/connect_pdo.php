363a8d31f9ddeb5bcd7001bb73bf47848efd4c79
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2024-02-26 00:51:23 -0500

    live database changes

--- includes/connect_pdo.php
+++ includes/connect_pdo.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=ek-contact_db;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=u802235977_ekam_portfolio;charset=utf8mb4";
 try {
-$connection = new PDO($dsn, 'root', '');
+$connection = new PDO($dsn, 'u802235977_ekam', 'Sidhu295');
 } catch (Exception $e) {
   error_log($e->getMessage());
   exit('unable to connect');
